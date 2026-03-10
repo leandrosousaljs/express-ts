d88623d60a29dbeeec9d0d01517a6fe1a18b2be5
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-03-10 16:48:02 -0300

    refactor: move pets data to module

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
 import express from 'express';
 import type { Express } from 'express';
 
+import { pets } from './data/pets';
+
 const app: Express = express();
 
-type Pet = {
-  name: string;
-  species: string;
-  adopted: boolean;
-  age: number;
-};
-
-const pets: Pet[] = [
-  {
-    name: 'Dimi',
-    species: 'Dog',
-    adopted: true,
-    age: 5,
-  },
-  {
-    name: 'Joaquina',
-    species: 'Cat',
-    adopted: true,
-    age: 3,
-  },
-  {
-    name: 'Pepito',
-    species: 'Parrot',
-    adopted: false,
-    age: 10,
-  },
-];
-
 app.get('/', (req, res) => {
-  res.json(pets);
+  res.json({ pets });
 });
 
 const PORT = process.env.PORT || 8000;
