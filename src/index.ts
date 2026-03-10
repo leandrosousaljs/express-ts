7c77bbcbfba1b61b5bb988d0beb2506d69ab780d
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-03-09 22:36:08 -0300

    chore: update .gitignore and package.json

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import type { Express } from 'express';
 
 const app: Express = express();
 
+app.get('/', (req, res) => {
+  res.json({});
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, (): void => console.log(`Server running on http://localhost:${PORT}`));
