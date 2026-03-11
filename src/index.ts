45857094233df0f6735db514db4585f3dc440808
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-03-11 15:22:04 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import type { Express, Request, Response } from 'express';
 
 import { pets } from './data/pets';
 
 const app: Express = express();
 
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+
 app.get('/', (req: Request, res: Response): void => {
   res.json({ pets });
 });
-
-const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, (): void => console.log(`Server running on http://localhost:${PORT}`));
