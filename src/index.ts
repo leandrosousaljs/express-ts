2e3918e9ff2ccd7534bc6c7231755304d339ef8f
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-03-11 15:43:07 -0300

    fix: export Pet type and update response type

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import type { Express, Request, Response } from 'express';
 
 import { pets } from './data/pets';
+import type { Pet } from './data/pets';
 
 const app: Express = express();
 
@@ -10,11 +11,11 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response): void => {
-  res.json({ pets });
+app.get('/', (req: Request, res: Response<Pet[]>): void => {
+  res.json(pets);
 });
 
-app.use((req: Request, res: Response): void => {
+app.use((req: Request, res: Response<{message: string}>): void => {
   res.status(404).json({ message: 'Endpoint not found' });
 });
 
